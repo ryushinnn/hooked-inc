e6742bd08b2ab92ed2b8fa26eac0d736785df34e
tiendeptrai <liw140402@gmail.com>
2024-07-26 16:22:23 +0700

    add ui: home, daily mission, shop (no code)

---
+++ Assets/_root/Scripts/UI/HomeUI/HomeUI.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assassin.Utils;
+using Assassin.Utils.ObjectPool;
+using TMPro;
+using UnityEngine;
+
+public class HomeUI : MonoBehaviour {
+    // [SerializeField] private TMP_Text _txtCurrentMoney;
+    //
+    // private void OnEnable() {
+    //     MessageDispatcher<MessageID.OnMoneyUpdated>.AddListener(OnMoneyUpdated);
+    // }
+    //
+    // private void OnDisable() {
+    //     MessageDispatcher<MessageID.OnMoneyUpdated>.RemoveListener(OnMoneyUpdated);
+    // }
+    //
+    // private void OnMoneyUpdated(float amount) {
+    //     _txtCurrentMoney.text = $"${amount:N0}";
+    // }
+}
