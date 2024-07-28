f3039143ff55a6e1e3b74814360040c45b014cb3
tienassassin <>
2024-07-28 15:46:10 +0700

    add ui: fishtank, improve ui: home (no code)

---
+++ Assets/_root/Scripts/UI/SafeAreaException.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Logger = Assassin.Utils.Logger;
+
+public class SafeAreaException : MonoBehaviour {
+    private Vector2 _defaultScreen = new(1080, 1920);
+    private RectTransform _panel;
+    
+    private void Awake() {
+        ApplyFullArea();
+    }
+
+    private void ApplyFullArea() {
+        _panel = GetComponent<RectTransform>();
+        StartCoroutine(DoApplyFullArea());
+    }
+
+    IEnumerator DoApplyFullArea() {
+        yield return new WaitForEndOfFrame();
+        FixPosition();
+        FixSizeDelta();
+    }
+    
+    private void FixPosition() {
+        _panel.position = Vector3.zero;
+    }
+
+    private void FixSizeDelta() {
+        var defRatio = _defaultScreen.y / _defaultScreen.x;
+        var scrRatio = (float)Screen.height / Screen.width;
+        if (defRatio < scrRatio) {
+            _panel.sizeDelta = new Vector2(_defaultScreen.x, _defaultScreen.x * scrRatio);
+        } else {
+            _panel.sizeDelta = new Vector2(_defaultScreen.y / scrRatio, _defaultScreen.y);
+        }
+    }
+}
